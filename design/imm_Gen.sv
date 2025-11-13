f6e59e415d128e17fd8877e557daddc49ed34f62
Pablo Nunes de Oliveira <151579198+PabloNunees@users.noreply.github.com>
2025-11-13 09:51:23 -0300

    Add I-type immediate arithmetic case in imm_Gen.sv

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -24,6 +24,10 @@ module imm_Gen (
         1'b0
       };
 
+      7'b0010011:
+      /*I-type immediate arithmetic*/
+      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+
       default: Imm_out = {32'b0};
 
     endcase
