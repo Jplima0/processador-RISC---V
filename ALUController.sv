74b3a848730177f9107f80227e22d50ddb90d1f4
João Pedro Lima de Araujo <jpla@cin.ufpe.br>
2025-12-04 15:39:42 -0300

    Refactor ALU operation assignments in ALUController

--- ALUController.sv
+++ ALUController.sv
@@ -32,12 +32,14 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-<<
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) || // R\I-<
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
       (ALUOp == 2'b11) || // JAL
       ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)); // R/I - SUb
 
   assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
       ((ALUOp == 2'b01) && (Funct3 == 3'b001)) || // BNE
       ((ALUOp == 2'b01) && (Funct3 == 3'b101)) || // BGE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
       (ALUOp == 2'b11) || // JAL
       ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
 endmodule
